716fe5a43038f7f4ef2acc9caca37a1931f98d50
Alex Hyams <111457482+tribayAlex@users.noreply.github.com>
2024-07-12 12:11:48 -0400

    Adapted for Enfocus Switch - Mac Only

--- Export Artboards to Files.jsx
+++ Export Artboards to Files.jsx
@@ -1,14 +1,27 @@
-﻿// artboards to files
-
-var document = app.activeDocument;
+﻿var document = $doc;
 var boards = document.artboards;
 
+if ($extension != "ai") {
+    $error = "This script is for Illustrator files only."
+    throw $error;
+}
+
 // loop through all artboards, make each one active and render before moving onto the next
-for(var i = 0; i < boards.length; i++) {
+for (var i = 0; i < boards.length; i++) {
     document.artboards.setActiveArtboardIndex(i);
     var saveOptions = new IllustratorSaveOptions();
-    var aiDoc = new File("~/Documents/artboard_"+(i+1).toString());
+    var thePath = Folder.temp + "/" + $filename; //temp output path
+    var aiDoc = new File(thePath);
     saveOptions.saveMultipleArtboards = true;
     saveOptions.artboardRange = (i+1).toString();
     document.saveAs(aiDoc, saveOptions);
-    }
+    thePath = thePath + "-" + boardNumber(i) + ".ai"; //path for each board
+    // alert(thePath);
+    $outfiles.push(thePath); //Send to Switch Output
+}
+
+function boardNumber (num) {
+    if (num + 1 < 10) num = "0" + (num + 1);
+    else num = (num + 1).toString();
+    return num;
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+Forked from [NTProductions](https://github.com/NTProductions/export-artboards-to-files).
+
+Meant for automated use in Enfocus Switch with the [Illustrator Configurator/App](https://www.enfocus.com/en/appstore/product/adobe-illustrator).
+Set the "command" property to "use script" and set the path to the jsx file.
+
+Note: Currently this script is for Mac only. Windows integration has a permissions issue with files being pulled from the os temp directory.
